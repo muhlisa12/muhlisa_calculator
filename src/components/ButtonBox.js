c0a7365d03a5c10686566f3eacbcecc8317e6f50
Muhlisa Dehqonboyeva <muhlisahonastrum2007@gmail.com>
2023-07-07 17:21:24 +0500

    my calculator

---
+++ src/components/ButtonBox.js
@@ -0,0 +1,8 @@
+// import ButtonBox from './components/ButtonBox'
+const ButtonBox = ({children}) => { 
+    return( 
+        <div className="buttonBox">{children}</div> 
+    ) 
+} 
+ 
+export default ButtonBox
---
+++ src/components/Wrapper.js
@@ -0,0 +1,9 @@
+// import Wrapper from"./components/Wrapper";
+
+const Wrapper = ({children}) => { 
+    return( 
+        <div className="wrapper">{children}</div> 
+    ) 
+} 
+ 
+export default Wrapper
